742b61681487115ab0ec627d888f904fb29ec8ae
m-miskevych <miskevychmarianna@gmail.com>
2024-10-03 17:11:17 +0200

    Add category seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Category.where(name: "Elektronika").first_or_create!
+Category.where(name: "Nabytok").first_or_create!
+Category.where(name: "Knihy").first_or_create!
+Category.where(name: "Reality").first_or_create!
+Category.where(name: "Sport").first_or_create!
+
+puts "Categories count: #{Category.count}"
