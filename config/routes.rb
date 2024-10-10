4c127e855cad32d8a82cb4c0ab8af6224d43e215
m-miskevych <miskevychmarianna@gmail.com>
2024-10-10 16:16:50 +0200

    category updates homework

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
+  #  ak sa pouziju vsetky actions, netreba only i
   resources :items, only: %i[index show new create edit update destroy]
-  resources :categories, only: %i[index show new create edit update destroy]
+  resources :categories
 end
