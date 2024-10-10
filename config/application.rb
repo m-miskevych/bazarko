8cd25c19de873b47771e27b3469fb2612233fab2
m-miskevych <miskevychmarianna@gmail.com>
2024-10-10 15:38:18 +0200

    Add i18n rails gem; add submit translation

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Bazarko
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.default_locale = :sk
   end
 end
