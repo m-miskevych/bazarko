e765d858d9126b8ee407c89c54fc025b3059134b
m-miskevych <miskevychmarianna@gmail.com>
2024-11-24 21:39:37 +0100

    Updating upload images; carousel

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,11 @@
 class Item < ApplicationRecord
+  has_many_attached :images do |attachable|
+    attachable.variant :thumb, resize_to_limit: [ 300, 300 ]
+    attachable.variant :normal, resize_to_limit: [ 800, 800 ]
+  end
+
   belongs_to :category
   belongs_to :user
-
-  has_many_attached :images
 
   validates :name, presence: true, length: { minimum: 10 }
   validates :price, presence: true
